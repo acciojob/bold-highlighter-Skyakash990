c52cdfebea2f8a9b256b2576c560cf53d648dc1a
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-17 15:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 function highlight() {
-    //Write your code here
-
-
+	
+     let strongElements = document.querySelectorAll('strong');
+	strongElements.forEach(element => {
+        element.style.color = "rgb(0, 128, 0)";
+    });
 }
 
 
 function return_normal() {
     //Write your code here
+	 let strongEl= document.querySelectorAll('strong');
 
+    strongEl.forEach(element => {
+        element.style.color = "rgb(0, 0, 0)";
     
 }
