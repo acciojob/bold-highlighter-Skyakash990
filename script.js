7d6a6376a18cee8d9c6fc4a7176979879008d388
Akash Nandoliya <99115930+Skyakash990@users.noreply.github.com>
2025-02-17 15:14:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 function highlight() {
-	
-     let strongElements = document.querySelectorAll('strong');
-	strongElements.forEach(element => {
-        element.style.color = "rgb(0, 128, 0)";
+    let strongElements = document.querySelectorAll('strong');
+    strongElements.forEach(element => {
+        element.style.color = "rgb(0, 128, 0)"; // Green color
     });
 }
 
-
 function return_normal() {
-    //Write your code here
-	 let strongEl= document.querySelectorAll('strong');
-
-    strongEl.forEach(element => {
-        element.style.color = "rgb(0, 0, 0)";
-    
-}
+    let strongElements = document.querySelectorAll('strong');
+    strongElements.forEach(element => {
+        element.style.color = "rgb(0, 0, 0)"; // Black color
+    });
+} // <-- Closing brace added
